2adf0be45a8e73edf28b1bfca176ddfca6c1d438
emmanuel.ngwandu <emmanuel.ngwandu@lands.go.tz>
2024-06-26 17:02:41 +0300

    adding nextui to remix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,48 @@
+import { Button, Checkbox, DatePicker, DateRangePicker, Input, Radio, RadioGroup, Select, SelectItem, Textarea, TimeInput } from "@nextui-org/react";
 import type { MetaFunction } from "@remix-run/node";
-
+import { ClientActionFunctionArgs, Form } from "@remix-run/react";
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Dakika Tisini" },
+    { name: "description", content: "Karibu Dakika tisini!" },
   ];
 };
 
+export async function clientAction({request}:ClientActionFunctionArgs) {
+  const formData = await request.formData();
+  console.log(Object.fromEntries(formData))
+  return null;  
+}
+
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className=" flex flex-col justify-center items-center min-h-screen font-sans p-4 bg-green-500">
+      <h1 className="text-3xl mb-2">Welcome to the Django</h1>
+      <Form method="post" className="flex flex-col gap-2 w-[400px]">
+            <Input label="Name" name="name" placeholder="Enter your name" type="text" isRequired ={true} />
+            <DatePicker name="birthDate" label="Birth date" className="min-w-[284px]"/>
+            <TimeInput label="Birth Time" name="birthTime" isRequired={true} />
+            <Input label="Email" name="email" placeholder="Enter your email" type="email" isRequired={true} />
+            <Select variant={"flat"} name="gender" label="Select an you gender" className="min-w-xs" isRequired={true} >
+                <SelectItem key={"Male"}>Male</SelectItem>
+                <SelectItem key={"Female"}>Female</SelectItem>
+            </Select>
+            <RadioGroup name={"city"} label="Select your favorite city" className="bg-white rounded-md p-2" isRequired={true}>
+                <Radio value="buenos-aires">Buenos Aires</Radio>
+                <Radio value="sydney">Sydney</Radio>
+                <Radio value="san-francisco">San Francisco</Radio>
+                <Radio value="london">London</Radio>
+                <Radio value="tokyo">Tokyo</Radio>
+            </RadioGroup>
+            <DateRangePicker label="Stay duration" className="min-w-xs" isRequired={true}/>
+            <Textarea
+              name="message"
+              label="Additional Message"
+              placeholder="Enter some words here"
+            />
+            <Checkbox name="isAccepted">Accept T&C</Checkbox>
+            <Button color="primary" type="submit">Save Changes</Button>
+      </Form>
     </div>
   );
 }
